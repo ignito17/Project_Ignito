d5360c514abeee8afc667691051ad3ba18c8c9b4
2320612 <prabhat3055@gmail.com>
2025-06-24 19:17:34 +0530

    Cloned the repo on office laptop Added a file hello.py which is to be moved to week_1 folders.

---
+++ hello.py
@@ -0,0 +1,7 @@
+# This program says hello and asks for my name.
+print("Hello World")
+print("What is your name?")             # ask for name
+myName=input()
+print("It is good to meet you, " + myName)
+print("The length of your name is:")
+print(len(myName))
